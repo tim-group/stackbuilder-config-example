088e3722ccffe110e849a2ea53e5b5f608cc9899
Richard Pearce <richard.pearce@timgroup.com>
2014-10-30 11:38:21 +0000

    rpearce: Add local dev environment

--- stack.rb
+++ stack.rb
@@ -116,5 +116,22 @@ env 'staging',
   instantiate_stack 'hello_db'
 end
 
+# Local Development Environment
+env 'dev',
+  {
+    # Primary location where stacks will be located
+    :primary_site=>'local',
+    # Unique reference for load balancers to use in this environment (to avoid conflicts ensure this number is unique in every environment
+    # FIXME: Ideally we would auto-generate this number
+    :lb_virtual_router_id=>1,
+    # Do Compute nodes in this environment support persistent storage? (default: truee)
+    :persistent_storage_supported=>false,
+    # Allow every machine to be destroyable (Overrides individiual allow_destroy settings for all Virtual Machines)
+    :every_machine_destroyable=>true,
+} do
+  instantiate_stack 'fabric'
+  instantiate_stack 'helloworld'
+  instantiate_stack 'hello_db'
+end
 
 # vim: set ts=2 sw=2 et :
