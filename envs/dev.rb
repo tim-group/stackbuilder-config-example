c869ecc35375e46c265bfceef91bda9a95194f99
Richard Pearce <richard.pearce@timgroup.com>
2016-11-01 17:53:14 +0000

    rpearce: These are going to be removed in future.

--- envs/dev.rb
+++ envs/dev.rb
@@ -6,10 +6,6 @@ env 'dev',
     # Unique reference for load balancers to use in this environment (to avoid conflicts ensure this number is unique in every environment
     # FIXME: Ideally we would auto-generate this number
     :lb_virtual_router_id=>1,
-    # Do Compute nodes in this environment support persistent storage? (default: truee)
-    :persistent_storage_supported=>false,
-    # Allow every machine to be destroyable (Overrides individiual allow_destroy settings for all Virtual Machines)
-    :every_machine_destroyable=>true,
 } do
   instantiate_stack 'hello-app'
   instantiate_stack 'hello-db'
