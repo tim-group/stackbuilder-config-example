a40b41d081b95c2bc3cb12c39473e86b7258e155
Richard Pearce <richard.pearce@timgroup.com>
2016-11-02 10:09:21 +0000

    rpearce: Make the examples richer

--- stacks/hello-app.rb
+++ stacks/hello-app.rb
@@ -6,11 +6,15 @@ stack 'hello-app' do
     self.application = 'JavaHttpRef'
     #Specify how many servers should be created (default: 2)
     self.instances = 2
+
+    self.jvm_args = '-Xms1G -Xmx1G'
+
     # Declare dependency on another stack (target)
     #   * The fqdn of servers in the source stack (on the prod network) will be appended to a list of dependant_instances on the target stack
     #   * The target stack will provide config_params to the source stack
-    depend_on = 'hellodb'
-    # For each machine in the stack
+    depend_on = 'hellodb', 'staging', :read_write if %w(staging).include? environment.name
+
+    # For each virtual machine in the stack
     each_machine do |machine|
       # Specify memory requirements (default: 2097152)
       machine.ram = '2097152'
@@ -18,6 +22,10 @@ stack 'hello-app' do
       machine.vcpus = '1'
       # Specify disk space for / partition (default: 3G)
       machine.modify_storage({ '/' => { :size => '5G' } })
+      # Specify that this machine should use the trusty gold image
+      machine.use_trusty
+      # Specify that this machine should go on a KVM host with the 'HP' tag (staging env only)
+      machine.allocation_tags << 'HP' if %w(staging).include? environment.name
     end
   end
 end
--- stacks/hello-proxy.rb
+++ stacks/hello-proxy.rb
@@ -1,10 +1,11 @@
 stack 'hello-proxy' do
   # Create a machineset with proxy_service capabilities
   proxy_service 'helloproxy' do
-    # Create a new Apache vhost for a specific application
-    vhost('helloapp') do
+    # Create a new Apache vhost for a specific <application>, <fqdn>
+    vhost('helloapp', 'hello.timgroup.com') do
       # Specify an additional server alias
       @aliases << 'helloworld.example.com'
+      # Send apache logging to syslog
       add_properties :apache_logs_to_syslog => true
     end
     # Allow the nat server to discover and auto-configure itself for these proxy servers
