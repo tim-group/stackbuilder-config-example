0515f3d480863ac8ecacd7887ca4b0ab79f0d8d3
Richard Pearce <richard.pearce@timgroup.com>
2017-03-30 14:42:37 +0100

    rpearce: Add missing do

--- stacks/nat-service.rb
+++ stacks/nat-service.rb
@@ -1,10 +1,11 @@
 stack 'nat-service' do
-  nat_service
+  nat_service do
     # For each virtual machine in the stack
     each_machine do |machine|
       # Specify that this machine should use the precise gold image
       machine.template(:precise)
     end
+  end
 end
 
 # vim: set ts=2 sw=2 et :
